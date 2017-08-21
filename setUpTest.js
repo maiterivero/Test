fea17d0e217242ef11842d208bd1ce35d8b48b3f
maiterivero <maite.rivero@homes.com>
2017-08-21 16:44:54 -0400

    Update setUpTest.js

--- setUpTest.js
+++ setUpTest.js
@@ -30,7 +30,7 @@ module.exports =
                 'chromeOptions': { args: ['test-type','ignore-certificate-errors','disable-extensions'] }
                     })
                     .forBrowser(setUp.browser)
-                    //.usingServer('http://localhost:4444/wd/hub')
+                    .usingServer('http://localhost:4444/wd/hub')
                     .build();
                 break;        
             default:
